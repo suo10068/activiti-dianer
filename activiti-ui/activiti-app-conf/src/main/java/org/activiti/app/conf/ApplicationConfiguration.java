ac2d1a65e9b73eade02b6a9b9ee178f6448faec0
dianer <dianer@tansun.com>
2018-05-10 20:40:22 +0800

    renew

--- activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/ApplicationConfiguration.java
+++ activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/ApplicationConfiguration.java
@@ -1,15 +1,3 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.activiti.app.conf;
 
 import org.springframework.context.annotation.*;
@@ -17,11 +5,8 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @PropertySources({
-	
-	@PropertySource("classpath:/META-INF/activiti-app/activiti-app.properties"),
+	@PropertySource("classpath:activiti-app.properties"),
 	@PropertySource(value = "classpath:activiti-app.properties", ignoreResourceNotFound = true),
-	@PropertySource(value = "file:activiti-app.properties", ignoreResourceNotFound = true),
-
 })
 @ComponentScan(basePackages = {
         "org.activiti.app.conf",
